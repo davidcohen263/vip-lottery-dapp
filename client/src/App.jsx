64a515e3a3ed638b1ec4d387c7af8362ecfd6479
davidcohen263 <dudicohen92@gmail.com>
2025-05-12 01:25:14 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import LotteryPage from "./pages/LotteryPage"; // ייבוא הדף של ההגרלה
+import LotteryPage from "./pages/LotteryPage";
+import UserDashboard from "./pages/UserDashboard";
+import AdminPanel from "./pages/AdminPanel";
+import Layout from "./components/Layout";
 
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/lottery/:id" element={<LotteryPage />} /> {/* תומך בלחיצה על הגרלה */}
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lottery/:id" element={<LotteryPage />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </Layout>
   </Router>
 );
 
