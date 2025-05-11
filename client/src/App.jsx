880b7260e179adaa1d3cd9e1c92facea06d91bff
davidcohen263 <dudicohen92@gmail.com>
2025-05-12 01:15:23 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import LotteryPage from "./pages/LotteryPage"; // ייבוא הדף של ההגרלה
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/lottery/:id" element={<LotteryPage />} /> {/* תומך בלחיצה על הגרלה */}
     </Routes>
   </Router>
 );
